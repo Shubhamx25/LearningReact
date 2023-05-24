bac8a2f15bcd0c4cf4a310d45a89b4104c723cd4
Shubhamx25 <shubhamy725p@gmail.com>
2023-05-24 18:18:11 +0530

    passing props but still kind of hard coded

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -4,12 +4,14 @@ import Card from './components/Card';
 const App = () => {
   return (
       <div className='card-container'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card
+          img = 'https://images.unsplash.com/photo-1593055497705-59a84c5928b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
+          title = 'Life lessons with Katie Holmes'
+          price = {136}
+          rating = {5.0}
+          noOfRating = {6}
+        />
+        
       </div>
   )
 }
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const card = () => {
+const card = (props) => {
   return (
     <div className='card'>
         <div className='card-img'>
-            <img src='https://images.unsplash.com/photo-1593055497705-59a84c5928b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'/>
+            <img src={props.img}/>
         </div>
         <div className='card-details'>
-            <p className='rate'><img src='https://img.icons8.com/?size=512&id=104&format=png' width='14px'/>&nbsp;5.0(6)&nbsp;&#9679;&nbsp;USA</p>
-            <h3 className='card-title'>Life lessons with Katie Holmes</h3>
-            <p><strong>From $136</strong>/person</p>
+            <p className='rate'><img src='https://img.icons8.com/?size=512&id=104&format=png' width='14px'/>&nbsp;{props.rating}({props.noOfRating})&nbsp;&#9679;&nbsp;USA</p>
+            <h3 className='card-title'>{props.title}</h3>
+            <p><strong>From ${props.price}</strong>/person</p>
         </div>
     </div>
   )
