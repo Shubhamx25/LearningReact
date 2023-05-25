e386d2fe36ff986efdc13e07420831fcc80f9dae
Shubhamx25 <shubhamy725p@gmail.com>
2023-05-25 11:26:01 +0530

    AirBnb clone completed. passing of props learned

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,19 +1,50 @@
 import React from 'react';
 import Card from './components/Card';
+import CardData from './components/CardData';
 
 const App = () => {
+
+let cardJsx = CardData.map(exp => {
+  return(
+  <Card 
+      // *********************Method 1: passing data*****************************
+       /*  img = {exp.img}
+        title = {exp.title}
+        price = {exp.price}
+        rating = {exp.rating}
+        noOfRating = {exp.noOfRating} */
+
+
+
+        // *********************Method 2: passing data*****************************
+       /*  item = {exp}   */
+        // passes the whole individual object but in props everywhere we have to explicitly metion props.item.propertyName 
+
+
+
+        // *********************Method 3: passing data*****************************
+        {...exp} 
+        // spread syntax feature from es6
+  />
+  )
+
+})
+console.log(cardJsx);
+
   return (
       <div className='card-container'>
-        <Card
+          {cardJsx} 
+      </div>
+  )
+}
+
+export default App;
+
+
+       /*  <Card
           img = 'https://images.unsplash.com/photo-1593055497705-59a84c5928b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
           title = 'Life lessons with Katie Holmes'
           price = {136}
           rating = {5.0}
           noOfRating = {6}
-        />
-        
-      </div>
-  )
-}
-
-export default App
+        /> */
---
+++ airbnb-clone/src/components/CardData.js
@@ -0,0 +1,50 @@
+export default  [
+    {
+        img :'https://images.unsplash.com/photo-1593055497705-59a84c5928b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+        title : 'Life lessons with Katie Holmes',
+        price : 136,
+        rating :5.0,
+        noOfRating:6
+
+    },
+    {
+        img :'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80',
+        title: 'An Amsterdam tour with Jim Malone',
+        price: 70,
+        rating:4.6,
+        noOfRating: 29
+
+    },
+    {
+        img :'https://images.unsplash.com/photo-1501386761578-eac5c94b800a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        title : 'A Jack Jones concert',
+        price : 156,
+        rating :4.9,
+        noOfRating:77
+
+    },
+    {
+        img :'https://images.unsplash.com/photo-1464746133101-a2c3f88e0dd9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1143&q=80',
+        title : 'A mountain hike with Nirmal Purja ',
+        price : 99,
+        rating :5.0,
+        noOfRating:3
+
+    },
+    {
+        img :'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80',
+        title: 'An Amsterdam tour with Jim Malone',
+        price: 70,
+        rating:4.6,
+        noOfRating: 29
+
+    },
+    {
+        img :'https://images.unsplash.com/photo-1501386761578-eac5c94b800a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        title : 'A Jack Jones concert',
+        price : 156,
+        rating :4.9,
+        noOfRating:77
+
+    }
+]
