98806e55244d8589de4466d09bf8502f3b402771
Shubhamx25 <shubhamy725p@gmail.com>
2023-05-30 17:34:15 +0530

    solo project 3 started

---
+++ solo_pro_3/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navigbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigbar />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ solo_pro_3/src/Components/Navbar.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Navbar,Container, Nav, NavDropdown } from 'react-bootstrap';
+import logoImg from '../assets/Logo-09 asthetic thumbnail.png';
+
+const Navigbar = () => {
+   
+  const [activeLink, setActiiveLink] = React.useState('home');  
+  const handleLinkActivation = (clickedLink) => {
+    setActiiveLink(clickedLink);
+  }
+
+
+  const[scrolled, setScrolled] = React.useState(false);
+  React.useEffect(() => {
+    let onScroll = () => {
+        if(window.scrollY > 50){
+            setScrolled(true);
+            console.log('scrolled')
+        }else{
+            setScrolled(false);
+        }
+    }
+   
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll',onScroll);
+  },[])
+
+  
+
+
+
+  return (
+    <div>
+        <Navbar  expand="lg" className={scrolled ? 'scrolled navbar-container' : 'navbar-container'}>
+            <Container>
+
+                <Navbar.Brand href="#home">
+                    <img src={logoImg} alt='logo' />  
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className='navbar-toggler-icon'></span>
+                </Navbar.Toggle>
+
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+
+                    <Nav.Link href="#home" className={activeLink == 'home' ? 'navbar-link liactive' : 'navbar-link'} onClick={() => handleLinkActivation('home')} >
+                        Home
+                    </Nav.Link>
+
+                    <Nav.Link href="#projects" className={activeLink == 'projects' ? 'navbar-link liactive' : 'navbar-link'} onClick={() => handleLinkActivation('projects')}>
+                        Projects
+                    </Nav.Link>
+
+                    <Nav.Link href="#link" className={activeLink == 'links' ? 'navbar-link liactive' : 'navbar-link'} onClick={() => handleLinkActivation('links')}>
+                        Links
+                    </Nav.Link>
+                    
+                    
+                </Nav>
+
+                <span>
+                    <div className='navbar-social-icons'>
+                        <a href='#'><img src='https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Instagram_svg-512.png'/></a>
+                        <a href='#'><img src='https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Linkedin_svg-512.png'/></a>
+                        <a href='#'><img src='https://cdn1.iconfinder.com/data/icons/social-media-circle-7/512/Circled_Messenger_svg3-512.png'/></a>
+                    </div>
+                </span>
+                <div>
+                    <button>Lets Connect</button>
+                </div>
+                
+                </Navbar.Collapse>
+            </Container>
+    </Navbar>
+    <div className='test'>howdy</div>
+    <div className='test'>howdy</div>
+    <div className='test'>howdy</div>
+    <div className='test'>howdy</div>
+    <div className='test'>howdy</div>
+    <div className='test'>howdy</div>
+    
+    </div>
+  )
+}
+
+export default Navigbar
